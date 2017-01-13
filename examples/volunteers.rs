fa628467c6c57085fa770329193f028716db1bfe
J/A <archer884@gmail.com>
2017-01-13 03:30:38 -0600

    First cut

---
+++ examples/volunteers.rs
@@ -0,0 +1,26 @@
+//! # Volunteers
+//!
+//! The concept behind this example is that of the cartoon cliche, the call for volunteers.
+//! Specifically, a commanding officer requests volunteers for a dangerous (or possibly just
+//! disgusting) mission by asking that interested parties take `n` steps forward. Instead,
+//! the more alert parties present simply take `n` steps back, leaving (usually) one
+//! unlucky and inattentive soul as the "volunteer."
+
+extern crate or_iter;
+
+use or_iter::OrIter;
+
+fn main() {
+    let contingent = vec!["Joe".to_string(), "Marty".to_string(), "Frank".to_string()];
+    let volunteers = contingent.into_iter().filter(|soldier| is_willing(soldier));
+    let volunteers = volunteers.or(|| "Joe".to_string()); // poor Joe!
+
+    for name in volunteers {
+        // only Joe appears here :(
+        println!("{}", name);
+    }
+}
+
+fn is_willing(_soldier: &str) -> bool {
+    false
+}
