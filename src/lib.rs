5a378036c443b5e798a6b0d14b6472e9f4a0e49a
J/A <archer884@gmail.com>
2017-01-13 13:55:01 -0600

    Add clippy, documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
+/// Describes a method for adapting any iterator with the `Or` adapter.
 pub trait OrIter
     where Self: Iterator + Sized
 {
+    /// Adapts an iterator using the `Or` iterator.
+    ///
+    /// In the event that the source iterator produces a `None` value without first having 
+    /// produced any other value, the `Or` iterator will produce a default value using the 
+    /// provided function. This default value will be produced once and only once.
     fn or<F: FnOnce() -> <Self as Iterator>::Item>(self, f: F) -> Or<Self, F>;
 }
 
